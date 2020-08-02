4c23e56bccef381b79f3fd8b68ba8ee00f1c9dbe
lit4g0 <lit4g0@gmail.com>
2020-08-02 02:25:27 +0200

    First Push

---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+
+    [SerializeField] KeyCode jump;
+    [SerializeField] KeyCode left;
+    [SerializeField] KeyCode right;
+    [SerializeField] KeyCode fire;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
