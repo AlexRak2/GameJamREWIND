9507b0b8fecbc0f0cb9af15d4609d32967b68e65
lit4g0 <lit4g0@gmail.com>
2020-08-02 23:46:20 +0200

    Start plants

---
+++ Assets/PlantMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantMovement : MonoBehaviour
+{
+    [SerializeField] GameObject plant;
+    [SerializeField] float speed = 0.1f;
+    [SerializeField] List<Transform> positions = new List<Transform>();
+    [SerializeField] bool rewinding = false;
+    [SerializeField] int currentTarget = 1;
+    [SerializeField] int prevTarget = 0;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (!rewinding)
+        {
+            plant.transform.position = Vector3.MoveTowards(plant.transform.position, positions[currentTarget].position, speed * Time.deltaTime);
+        }
+        else
+        {
+            plant.transform.position = Vector3.MoveTowards(plant.transform.position,, positions[prevTarget].position, speed * Time.deltaTime);
+        }
+    }
+
+    public void SetRewind(bool rewindIn)
+    {
+        rewinding = rewindIn;
+    }
+}
