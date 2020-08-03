b3ca8cacd2a349f34b4bb1904e1f0a4318c867d1
lit4g0 <lit4g0@gmail.com>
2020-08-03 19:59:13 +0200

    Black n white

--- Assets/Scripts/Spells.cs
+++ Assets/Scripts/Spells.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class Spells : MonoBehaviour
 {
@@ -29,10 +31,16 @@ public class Spells : MonoBehaviour
     float chargeTime = 0f;
     float maxChargeTime = 0f;
     float chargeValue = 0f;
+    [SerializeField] Volume postProcessVolume;
+    Bloom bloom;
+    ColorAdjustments colorAdjust;
+    float bwAmount = 0f;
 
     void Start()
     {
         plants.AddRange(FindObjectsOfType<PlantMovement>());
+        postProcessVolume.profile.TryGet(out colorAdjust);
+
     }
 
     void Update()
@@ -97,8 +105,11 @@ public class Spells : MonoBehaviour
                 foreach(PlantMovement plant in plants)
                 {
                     plant.SetRewind(true);
+                    //bwAmount = Mathf.Lerp(0f, -100f, 5f);
+
                 }
             }
+            colorAdjust.saturation.Override(Mathf.Lerp(0f, -100f, chargeTime));
             rewindCasting = true;
             spellText.text = "Rew.";
             maxChargeTime = rewindChargeTime;
@@ -107,14 +118,13 @@ public class Spells : MonoBehaviour
             spellImageMask.fillAmount = chargeValue;
         }
 
-
-
         if (Input.GetKeyUp(rewindKey) && rewindCasting)
         {
             foreach (PlantMovement plant in plants)
             {
                 plant.SetRewind(false);
             }
+            colorAdjust.saturation.Override(0f);
             print("Rewinded for a value of: " + chargeValue + "!");
             spellText.text = "";
             rewindCasting = false;
