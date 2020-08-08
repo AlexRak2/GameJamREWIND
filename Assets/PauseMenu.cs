8b53f7cb447fd76d019bc5ef76eaea825e8ce99b
lit4g0 <lit4g0@gmail.com>
2020-08-08 06:11:41 +0200

    pause menu prefab

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    bool open = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !open)
+        {
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+            open = true;
+            print("Pause");
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && open)
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+            open = false;
+            print("resume");
+        }
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
